e138f6350b0e12cc5ea608b6802a1108165e498c
Kerols Shafik <kerols.shafik6@gmail.com>
2026-03-17 15:01:25 +0200

    handle

---
+++ database/migrations/2026_03_17_145850_create_visit_product_price_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitProductPriceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql2')->create('rose_visit_product_prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('visit_id');
+            $table->unsignedBigInteger('store_id');
+            $table->unsignedBigInteger('product_id');
+            $table->decimal('price', 12, 2);
+            $table->timestamps();
+
+            $table->unique(['visit_id', 'store_id', 'product_id'], 'visit_store_product_unique');
+            $table->index(['visit_id', 'store_id'], 'visit_store_index');
+            $table->index(['product_id'], 'product_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rose_visit_product_prices');
+    }
+}
